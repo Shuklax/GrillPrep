85901ac60f6582b6d6d2dffec897c0fa912d204b
Shuklax <harshshuklaknp@gmail.com>
2024-10-13 01:00:08 +0530

    adding the paths/mappings of company logos - Homepage v2

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import dayjs from 'dayjs'
+import Image from "next/image";
 
-const InterviewCard = () => {
+
+const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+    const feedback = null as Feedback | null;
+
+    const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
+    const formattedDate = dayjs(feedback?.createdAt || createdAt || Date.now()).format('YYYY-MM-DD HH:mm:ss')
+
     return (
-        <div>InterviewCard</div>
+        <div className="card-border w-[360px] max-sm:w-full min-h-96">
+            <div className="card-interview">
+                <div>
+                    <div className="absolute top-8 right-0 w-fity px-4 py-2 rounded-bl-lg bg-light-600">
+                        <p className="badge-text">{normalizedType}</p>
+                    </div>
+
+                    <Image src={getRandomInterviewCover()} alt="coverImage" width={90} height={90}
+                           className="rounded-full object-fit size-[90px]"/>
+                </div>
+            </div>
+        </div>
     )
 }
 export default InterviewCard
