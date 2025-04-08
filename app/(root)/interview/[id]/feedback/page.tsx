a5125591ff124689a00f5f192e4c55f613f4f085
Shuklax <harshshuklaknp@gmail.com>
2025-04-08 17:57:35 +0530

    completes the MVP

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -4,8 +4,10 @@ import {getFeedbackByInterviewId, getInterviewById} from "@/lib/actions/general.
 import {redirect} from "next/navigation";
 import Image from "next/image";
 import dayjs from "dayjs";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
-const Page = async ({params}: RouteParams) => {
+const Feedback = async ({params}: RouteParams) => {
     const {id} = await params;
     const user = await getCurrentUser();
 
@@ -66,8 +68,46 @@ const Page = async ({params}: RouteParams) => {
                 ))}
             </div>
 
+            <div className="flex flex-col gap-3">
+                <h3>Strengths</h3>
+                <ul>
+                    {feedback?.strengths?.map((strength, index) => (
+                        <li key={index}>{strength}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="flex flex-col gap-3">
+                <h3>
+                    Areas of Improvement
+                </h3>
+                <ul>
+                    {feedback?.areasForImprovement?.map((area, index) => (
+                        <li key={index}>{area}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="buttons">
+                <Button className="btn-secondary flex-1">
+                    <Link href="/" className="flex w-full justify-center">
+                        <p className="text-sm font-semibold text-primary-200 text-center">
+                            Back to Dashboard
+                        </p>
+                    </Link>
+                </Button>
+
+                <Button className="btn-primary flex-1">
+                    <Link href={`/interview/${id}`}
+                          className="flex w-full justify-center">
+                        <p className="text-sm font-semibold text-black text-center">
+                            Retake Interview
+                        </p>
+                    </Link>
+                </Button>
+            </div>
 
         </section>
     )
 }
-export default Page
+export default Feedback
